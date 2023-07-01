335600e91a001de671651e2d26a0ef81d9e6b162
hernanfuria <hernan.furiasse@hotmail.com>
2023-07-01 14:04:56 -0300

    Esqueleto de comprimir.sh
    Agregue las funciones que van a formar parte del
    proceso, pero sin implementacion.
    
    Signed-off-by: hernanfuria <hernan.furiasse@hotmail.com>

--- src/comprimir.sh
+++ src/comprimir.sh
@@ -8,4 +8,47 @@
 
 # modificacion de prueba en rama comprimir
 
+function nombres_todas_img {
+	# Esta funcion crea un archivo assets/comp/nombres_todos con los nombres de todas las imagenes del dataset
+	
+	echo nombres_todas_img
+}
+
+
+function nombres_validos_img {
+	# Esta funcion crea un archivo assets/comp/nombres_validos con los nombres de las imagenes procesadas
+	
+	echo nombres_todas_img
+}
+
+
+function nombres_finalizados_a {
+	# Esta funcion crea un archivo assets/comp/nombres_a con los nombres validos terminados en "a"
+
+	echo nombres_finalizados_a
+}
+
+
+function comprimir_todo {
+	# Esta funcion comprime:
+	# - assets/comp/nombres_todos
+	# - assets/comp/nombres_validos
+	# - assets/comp/nombres_a
+	# - assets/proc/*.jpeg
+	# en assets/comp/results.tar.gz y elimina el resto del contenido de assets/comp
+
+	echo comprimir_todo
+}
+
+function run {
+	# Esta funcion ejecuta el proceso en orden
+	
+	nombres_todas_img
+	nombres_validos_img
+	nombres_finalizados_a
+	comprimir_todo
+}
+
+run
+
 exit 0
