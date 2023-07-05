2114ed7da64a58826eaf4e450523eba7df6fcdf2
Leonel Perotto <leonelperotto@gmail.com>
2023-07-04 22:21:40 -0300

    1er commit
    Cree las funciones para descomprimir los archivos, me falta hacer la
    verificaion de sumas y arreglar algunos errores que creo que son de
    algunas variables.
    
    Signed-off-by: Leonel Perotto <leonelperotto@gmail.com>

--- src/descargar.sh
+++ src/descargar.sh
@@ -7,4 +7,56 @@
 
 # cambio en rama descargar
 
-exit 0
+SCRIPT_PATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"  # directorio del archivo actual, no entiendo del todo como funciona
+ASSETS_PATH="$SCRIPT_PATH/../assets"  # ruta de directorio de assets
+
+function verificar_arch {
+	#esta funcion verifica si existen el archivo.
+	#caso contrario se notifica al usuario.
+	cd "$SCRIPT_PATH"
+	local ARCH=$1
+	if [ ! -f "$ARCH" ]; then
+		echo "el archivo"
+		echo " "
+		echo "$ARCH"
+		echo " "
+		echo "no existe"
+		exit 1
+	fi
+}
+
+function desc_arch {
+	#funcion para descomprimir archivos de imagen que esten en el directorio assets/dataset/.
+	#los archivos descomprimidos seran guardados en el directorio assets/dataset/raw
+	cd "ASSETS_PATH/dataset/raw"
+	local ARCHCOMP=$1
+	tar -xzvf "$ARCHCOMP"
+}
+
+function descomprimir {
+	#funcion principal.
+	#Primero verifica que se hallan pasado 2 argumentos.
+	if [ $# -ne 2 ]; then
+		echo "Se deben pasar 2 argumentos"
+		exit 1
+	fi
+
+	#asignacion de argumentos a variables.
+
+	local ARCHCOMP="$ASSETS_PATH/dataset/$1"
+	local ARCHSUM="$ASSETS_PATH/dataset/$2"
+
+	#verificamos que los archivos existan con la funcion verificar_arch.
+
+	verificar_arch "$ARCHCOMP"
+	verificar_arch "$ARCHSUM"
+
+	#verificar que coincidan la suma de los archivos comprimidos con la de los descomprimidos.
+	#VER
+
+	#mostrar informacion
+
+	echo "Archivos descomprimidos: "
+	echo " "
+	ls -la "$ASSETS_PATH/dataset/raw"
+}	
