7308a2b6c6ac103004d91613abd03c3604eb6c22
SantiGuzman14 <santi.guzman1408@gmail.com>
2023-07-06 13:09:21 -0300

    Commit de Prueba
    A ver como funciona
    
    Signed-off-by: SantiGuzman14 <santi.guzman1408@gmail.com>

--- src/procesar.sh
+++ src/procesar.sh
@@ -8,4 +8,11 @@
 
 # modificacion en rama procesar
 
+
+SCRIPT_PATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+ASSETS_PATH="$SCRIPT_PATH/../assets"
+
+convert entrada.jpg -gravity center -resize 512x512+0+0 \
+-extent 512x512 salida.jpg
+
 exit 0
